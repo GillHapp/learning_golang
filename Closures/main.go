0c4834c81cf1c65093b5b9453e5b1c04c90507fa
happy <gillharpreetsingh211@gmail.com>
2025-02-28 20:14:54 +0530

    closure

--- Closures/main.go
+++ Closures/main.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
-func multiplier(factor int) func(int) int {
-    return func(x int) int {
-        return x * factor // Factor is remembered
-    }
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
 
 func main() {
-    double := multiplier(2) // Closure remembers factor = 2
-    triple := multiplier(3) // Closure remembers factor = 3
 
-    fmt.Println(double(5)) // Output: 10 (5 * 2)
-    fmt.Println(triple(5)) // Output: 15 (5 * 3)
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
 }
